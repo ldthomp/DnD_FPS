f66026eb31dfe86f62ae92b20ac11279b1ec0f2b
Adam Lee <alee8648@gmail.com>
2020-08-24 16:35:02 +0100

    Initial Commit - gitignore - enemy health - weapon - enemy moves - FP setup - sandbox scene - imported assets

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " attacking " + target.name);
+    }
+}
---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+using System;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField] Camera FPcamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 1f;
+    [SerializeField] ParticleSystem arrowShot;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayShootingEffect();
+        ProcessRaycast();
+
+    }
+
+    private void PlayShootingEffect()
+    {
+        arrowShot.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPcamera.transform.position, FPcamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit this thing: " + hit.transform.name);
+            //todo add some hit effect
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
