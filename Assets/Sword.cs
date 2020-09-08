3f677b8ecc133c305981e6d3ecbddbbe309ced34
Liam Thompson <l.d.thompson215@gmail.com>
2020-09-08 17:43:00 +0100

    Sword animation added - consider animation events for dealing damage rather than just box collider

--- Assets/Sword.cs
+++ Assets/Sword.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    bool canShoot = true;
+    [SerializeField] float damage = 50f;
 
-    //bool canShoot = true;
-//    void Update()
-//    {
-//      if (canShoot == true)
-            //{ }
-        //if (Input.GetMouseButtonDown(0) && canShoot == true)
-//        {
-//            AnimateSword();
-//        }
-//    }
-
-//    public void AnimateSword()
-//    {
-//        if (ammoSlot.GetCurrentAmmo() >= 1)
-//        {
-//        }
-//    }
-//
+    void Update()
+    {
+        if (canShoot == true)
+        {
+            if (Input.GetMouseButton(0) && canShoot == true) // while left click is held down
+            {
+                GetComponent<Animator>().SetBool("CanAttack", true);
+            }
+            if (Input.GetMouseButtonUp(0)) // when left click is released
+            {
+                GetComponent<Animator>().SetBool("CanAttack", false);
+            } // move back to idle state
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collide (name) : " + collision.gameObject.name);
+        EnemyHealth target = collision.transform.GetComponent<EnemyHealth>();
+        if (target == null) return;
+        target.TakeDamage(damage);
+    }
 }
